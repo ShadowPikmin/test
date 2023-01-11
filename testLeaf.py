cbb221f1c9a2ed3dd25057558ccdee08b3aa5886
ShadowPikmin <108579977+ShadowPikmin@users.noreply.github.com>
2023-01-11 12:29:10 -0600

    Create testLeaf.py

---
+++ testLeaf.py
@@ -0,0 +1,13 @@
+#Create a Pikmin Class
+class Pikmin:
+  def __init__(self, type, stage, name):
+    self.type = type
+    self.stage = stage
+    self.name = name 
+   
+  def getInfo():
+    print("This is a " + self.type + "Pikmin named " + self.name " that is currently a " + self.stage + " pikmin")
+    
+    
+pikmin = Pikmin("red", "leaf", "Steve")
+pikmin.getInfo()
